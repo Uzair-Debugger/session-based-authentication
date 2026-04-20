f032ac1ab021df4dc71f7c85ba22a08f86cce717
Uzair-Debugger <smuzair14cse@gmail.com>
2026-04-20 22:27:43 +0500

    Implemented Password hashing | Successful signup and storing user data in users table

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,13 +1,32 @@
+import bcrypt from "bcrypt"
+import User from "../models/user.model.js";
 import type {  signUpInterface } from "../interfaces/interface.js";
-import dbConfig from "../config/dbConfig.js";
 import type { Request, Response } from "express";
 
-const handleSignup = (req: Request<{},{}, signUpInterface>, res:Response) =>{
-    const {name, email} = req.body
-    console.log(`Name is ${name}, email is ${email}`)
-   
+const handleSignup =async (req: Request<{},{}, signUpInterface>, res:Response) =>{
+    const {name, email, password} = req.body
+    
+    if(!name || !email || !password){
+        
+        return res.status(400).json({"message": `Missing input! Please fill all input fields`})
+    }
 
-    return res.json({"message": `Received name: ${name}, email: ${email}`})   
+    const existingUser= await User.findOne({where: {email}});
+    
+    if(existingUser){
+        return res.status(400).json({"message": "User already exist"})
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashPassword
+    });
+    
+    return res.status(201).json({"message": "User signup Successfully"})
+
 }
 
 export default handleSignup;
