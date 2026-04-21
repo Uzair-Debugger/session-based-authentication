df9f7259201ef3fb902cb68503544d7b54367091
Uzair-Debugger <smuzair14cse@gmail.com>
2026-04-21 21:15:27 +0500

    Added login routes & handleLogin()

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import handleSignup from "../services/auth.services.js";
+import handleSignup, { handleLogin } from "../services/auth.services.js";
 
 export const authRouter = express.Router()
 
-authRouter.post('/signup', handleSignup)
+authRouter.post('/signup', handleSignup);
+authRouter.post('/login', handleLogin);
--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -4,8 +4,10 @@ import type {  signUpInterface } from "../interfaces/interface.js";
 import type { Request, Response } from "express";
 
 const handleSignup =async (req: Request<{},{}, signUpInterface>, res:Response) =>{
-    const {name, email, password} = req.body
-    
+    try {
+        
+        const {name, email, password} = req.body
+        
     if(!name || !email || !password){
         
         return res.status(400).json({"message": `Missing input! Please fill all input fields`})
@@ -24,9 +26,51 @@ const handleSignup =async (req: Request<{},{}, signUpInterface>, res:Response) =
         email,
         password: hashPassword
     });
-
+    
     return res.status(201).json({"message": "User signup Successfully", "userId": user.id})
-
+} catch (error) {
+    console.log(``)
+}
+    
 }
 
 export default handleSignup;
+
+// LOGIN
+export const handleLogin = async (req: Request<{},{}, signUpInterface>, res:Response) => {
+    try {
+        const { email, password } = req.body;
+
+        // 1. Validate
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields required" });
+        }
+
+        // 2. Find user
+        const user = await User.findOne({ where: { email } });
+        
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        console.log("User JSON:", user.toJSON());
+
+        // 3. Compare password
+        console.log(`User password: ${password}, Actual Password ${user.password}`)
+        const isMatch = await bcrypt.compare(password, user.toJSON().password);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        // 4. Create session
+        req.session.userId = user.id;
+
+        return res.json({
+            message: "Login successful",
+        });
+
+    } catch (error) {
+        console.error("Error:",error);
+        return res.status(500).json({ message: "Server error" });
+    }
+};
